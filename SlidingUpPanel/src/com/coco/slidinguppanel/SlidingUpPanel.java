e39007a4b72a12713cb5c42aaf332111ebe28e24
justin <zzhouj@yahoo.com>
2014-01-10 13:53:20 +0800

    add license

--- SlidingUpPanel/src/com/coco/slidinguppanel/SlidingUpPanel.java
+++ SlidingUpPanel/src/com/coco/slidinguppanel/SlidingUpPanel.java
@@ -1,5 +1,28 @@
 package com.coco.slidinguppanel;
 
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2014 justin
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import android.content.Context;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.VelocityTrackerCompat;
@@ -17,7 +40,7 @@ import android.view.animation.BounceInterpolator;
 import android.widget.Scroller;
 
 /**
- * Zaker style sliding up panel, which can only contain on child view.
+ * Zaker style sliding up panel, using as zaker's cover.
  */
 public class SlidingUpPanel extends ViewGroup {
 	private static final String TAG = "SlidingUpPanel";
